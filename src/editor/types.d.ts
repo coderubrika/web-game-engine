9a1e88b89ed7b17f881c4d6e4b5dc99f02c50211
DESKTOP-R2LSA46\Anton <coderubrika@gmail.com>
2022-06-14 17:06:46 +0300

    glsl setup started

--- src/editor/types.d.ts
+++ src/editor/types.d.ts
@@ -4,3 +4,18 @@ export interface WindowProps {
   header: string
   children?: React.ReactNode
 }
+
+declare module '*.glsl' {
+  const value: string
+  export default value
+}
+
+declare module '*.frag' {
+  const value: string
+  export default value
+}
+
+declare module '*.vert' {
+  const value: string
+  export default value
+}
