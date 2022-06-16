27bc7bb63d6960b46e59835cfe5fff2e5e2def1f
DESKTOP-R2LSA46\Anton <coderubrika@gmail.com>
2022-06-17 00:38:23 +0300

    simple 3d

--- src/editor/components/Game.tsx
+++ src/editor/components/Game.tsx
@@ -1,12 +1,17 @@
+import { Canvas } from '@react-three/fiber'
 import React from 'react'
+import Box from './primitives/Box'
 import Window from './ui/Window'
 
 export default function Game() {
   return (
     <Window header={'Game'}>
-      <canvas id='canvas'>
-        
-      </canvas>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
     </Window>
   )
 }
--- src/editor/components/Scene.tsx
+++ src/editor/components/Scene.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { Suspense, useRef, useState } from 'react'
+import { Canvas, useFrame } from "@react-three/fiber"
+import { Stats, OrbitControls } from "@react-three/drei"
 import Window from './ui/Window'
+import Box from './primitives/Box'
 
 export default function Scene() {
+
   return (
     <Window header={'Scene'}>
-      <canvas id='canvas'>
-        
-      </canvas>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
     </Window>
   )
 }
